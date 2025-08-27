3d3c7d892b8ba648e7499df9f9991965e87e4b1d
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2025-08-27 22:41:35 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { NotFound } from './components/not-found/not-found';
 import { Register } from './components/users/register/register';
 import { Profile } from './components/users/profile/profile';
 
+import { ProductsComponent } from './components/product/products/products.component';
+import { ProductdetailsComponent } from './components/product/productdetails/productdetails.component';
+import { AddProductComponent } from './components/product/addproduct/addproduct.component';
+import { UpdateproductComponent } from './components/product/updateproduct/updateproduct.component';
+import { DeleteProductComponent } from './components/product/deleteproduct/deleteproduct.component';
 export const routes: Routes = [
     {
         path: 'login',
@@ -14,13 +19,41 @@ export const routes: Routes = [
         component: Register
     },
      { path: '',
-       redirectTo: '/login', 
-       pathMatch: 'full' 
+       redirectTo: '/login',
+       pathMatch: 'full'
     },
+
     {
         path: 'profile',
         component: Profile
     },
+    {
+    path: 'products',
+    component: ProductsComponent,
+    title: 'Products Page'
+  },
+  { path: 'details/:id',
+    component: ProductdetailsComponent,
+    title: 'Product Details' }
+,
+{
+  path: 'add',
+  component: AddProductComponent,
+  title: 'Add Product'
+},
+
+{
+  path: 'update/:id',
+  component: UpdateproductComponent,
+  title: 'Update Product'
+},
+{
+  path: 'delete/:id',
+  component: DeleteProductComponent,
+  title: 'Delete Product'
+},
+
+
     {
         path:'**',
         component:NotFound
