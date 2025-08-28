8e6fe803af6cdf1d0eca04fc76c16ed94279c6a9
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2025-08-28 09:50:22 +0300

    Update addproduct.component.ts

--- src/app/components/product/addproduct/addproduct.component.ts
+++ src/app/components/product/addproduct/addproduct.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProductService } from '../../../services/product.service';
-
+import { AuthService } from '../../../services/auth-service';  // ✅ استدعاء AuthService
 
 @Component({
   selector: 'app-addproduct',
@@ -16,17 +16,23 @@ export class AddProductComponent {
   addForm: FormGroup;
   submitted = false;
   selectedFile: File | null = null;
+  isAdminUser: boolean = false;
 
-  constructor(private fb: FormBuilder, private productService: ProductService) {
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductService,
+    private authService: AuthService
+  ) {
     this.addForm = this.fb.group({
-  name: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/^[A-Za-z\s]+$/)]],
-  description: ['', [Validators.required, Validators.minLength(5)]],
-  price: ['', [Validators.required, Validators.min(0)]],
-  stock: ['', [Validators.required, Validators.min(0)]],
-  category: ["", Validators.required],
-  image: ['']
-});
+      name: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/^[A-Za-z\s]+$/)]],
+      description: ['', [Validators.required, Validators.minLength(5)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      stock: ['', [Validators.required, Validators.min(0)]],
+      category: ["", Validators.required],
+      image: ['']
+    });
 
+    this.isAdminUser = this.authService.isAdmin();
   }
 
   get formControls() {
@@ -41,6 +47,12 @@ export class AddProductComponent {
 
   onSubmit() {
     this.submitted = true;
+
+    if (!this.isAdminUser) {
+      alert("🚫 You are not authorized to add products!");
+      return;
+    }
+
     if (this.addForm.valid) {
       const formData = new FormData();
       formData.append('name', this.addForm.value.name);
@@ -49,8 +61,6 @@ export class AddProductComponent {
       formData.append('stock', this.addForm.value.stock);
       formData.append("category", this.addForm.get("category")!.value);
       if (this.selectedFile) formData.append('image', this.selectedFile, this.selectedFile.name);
-
-
 
       this.productService.addProduct(formData).subscribe({
         next: (res) => {
